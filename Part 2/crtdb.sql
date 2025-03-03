824993b1a063afa35576d922595c3f5f1e7b688d
vgnodle <124189565+vgnodle@users.noreply.github.com>
2025-03-02 18:50:40 -0700

    Removed Autoincrement

--- Part 2/crtdb.sql
+++ Part 2/crtdb.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE Member (
-memberId INTEGER PRIMARY KEY AUTOINCREMENT,
+memberId INTEGER PRIMARY KEY,
 name VARCHAR(50),
 email VARCHAR(50) NOT NULL,
 phone VARCHAR(15),
@@ -11,20 +11,20 @@ membershipStartDate DATE NOT NULL,
 membershipEndDate DATE NOT NULL CHECK (membershipEndDate >= membershipStartDate)
 );
 CREATE TABLE Instructor (
-instructorId INTEGER PRIMARY KEY AUTOINCREMENT,
+instructorId INTEGER PRIMARY KEY,
 name VARCHAR(50),
 specialty VARCHAR(50),
 phone VARCHAR(15),
 email VARCHAR(100) NOT NULL
 );
 CREATE TABLE GymFacility (
-gymId INTEGER PRIMARY KEY AUTOINCREMENT,
+gymId INTEGER PRIMARY KEY,
 location VARCHAR(100),
 phone VARCHAR(50),
 manager VARCHAR(50)
 );
 CREATE TABLE Class (
-classId INTEGER PRIMARY KEY AUTOINCREMENT,
+classId INTEGER PRIMARY KEY,
 className VARCHAR(50),
 classType VARCHAR(20) NOT NULL CHECK (classType IN ('Yoga', 'Zumba', 'HIIT', 'Weights', 'Swim')),
 duration INTEGER NOT NULL,
@@ -35,7 +35,7 @@ FOREIGN KEY (instructorId) REFERENCES Instructor(instructorId),
 FOREIGN KEY (gymId) REFERENCES GymFacility(gymId)
 );
 CREATE TABLE Equipment (
-equipmentId INTEGER PRIMARY KEY AUTOINCREMENT,
+equipmentId INTEGER PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 type VARCHAR(30) NOT NULL CHECK (type IN ('Cardio', 'Strength', 'Flexibility', 'Recovery', 'Safety')),
 quantity INTEGER,
@@ -43,12 +43,12 @@ gymId INTEGER,
 FOREIGN KEY (gymId) REFERENCES GymFacility(gymId)
 );
 CREATE TABLE MembershipPlan (
-planId INTEGER PRIMARY KEY AUTOINCREMENT,
+planId INTEGER PRIMARY KEY,
 planType VARCHAR(20) NOT NULL CHECK (planType IN ('Monthly', 'Annual', 'Punch Card', 'Weekly', 'Discount')),
 cost NUMERIC NOT NULL
 );
 CREATE TABLE Payment (
-paymentId INTEGER PRIMARY KEY AUTOINCREMENT,
+paymentId INTEGER PRIMARY KEY,
 memberId INTEGER,
 planId INTEGER,
 amountPaid REAL NOT NULL,
